44f2fc954ff2d2a42684e3297dc34b534e1d2785
Keith Irwin <mail@keithirwin.us>
2017-09-01 04:08:17 +0000

    Fixed contract and tests, except for gas usage

--- contracts/mixer.sol
+++ contracts/mixer.sol
@@ -3,46 +3,43 @@ pragma solidity ^0.4.11;
 /// @title Mix your ether.
 contract Mixer {
 	
-	/// Total amount of ether in the mixer
-	uint public total;
+	/// Creator of this contract
+	address owner;
+	// Set creator of this contract
+	function Mixer() {
+		owner = msg.sender;
+	}
 	
 	/// Users of the mixer, and the amount deposited
 	mapping(address => uint) balances;
 	
-	/// Event that will fire when the balance changes
-	event total_changed(uint newTotal);
-
+	function showUserBalance(address user) returns(uint) {
+		require(msg.sender == owner);
+		return balances[user];
+	}
+	
 	/// Deposit ether into the mixer
 	function deposit() payable {
 		
-		/// Add deposit to mixer
+		// Add deposit to mixer
 		balances[msg.sender] += msg.value;
-		
-		/// Increase total value of mixer
-		total += msg.value;
-		
-		/// Notify about change to balance
-		total_changed(total);
 		
 	}
 	
 	/// Withdraw ether from the mixer
 	function withdraw(address output, uint amount) {
 		
-		/// Ensure the user has deposited that much
+		// Ensure the user has deposited that much
 		require(amount <= balances[msg.sender]);
 		
 		// Subtract from that user's balance
 		balances[msg.sender] -= amount;
 		
-		// Subtract from mixer balance
-		total -= amount;
-		
-		// Notify about the change
-		total_changed(total);
-		
 		// Pay the user's new address
-		output.transfer(amount);
+		if (!output.send(amount)) {
+			// Return the balance if it fails
+			balances[msg.sender] += amount;
+		}
 		
 	}
 	
--- test/mixer.js
+++ test/mixer.js
@@ -1,61 +1,136 @@
-var Mixer = artifacts.require('Mixer');
+const Mixer = artifacts.require('Mixer');
+
+// Aliases for web3 functions
+function getBalance(address) {
+	return web3.eth.getBalance(address);
+}
+function toEth(wei) {
+	return web3.fromWei(wei,'ether');
+}
+function toWei(eth) {
+	return web3.toWei(eth,'ether');
+}
 
 contract('Mixer', (accounts)=>{
+		var meta, 
+			accountInitial, contractInitial, userBalanceInitial,
+			accountFinal, contractFinal, userBalanceFinal;
+	
+	const depositAmount = 10,
+		withdrawAmount = 5;
 
-	it("should deposit eth from first account", ()=>{
-		var instance;
+	it(`should let the first account deposit ${depositAmount} eth`, ()=>{
 		return Mixer.deployed().then( (instance)=>{
 			meta = instance;
-			// Check inital balance
-			return meta.total.call();
-		})
-		.then( (balance)=>{
-			assert.equal(balance, 0, "Total balance didn't initiate at 0");
+			
+			// Check initial balances
+			accountInitial = toEth(getBalance(accounts[1]));
+			contractInitial = toEth(getBalance(meta.address));
+			return meta.showUserBalance.call(accounts[1]);
+		}).then( (userBalance)=>{
+			userBalanceInitial = toEth(userBalance);
+			
 			// Perform deposit
-			return meta.deposit({from:accounts[0], value:10});
-		})
-		.then( ()=>{
-			// Check final balance
-			return meta.total.call();
-		})
-		.then( (balance)=>{
-			assert.equal(balance.toNumber(), 10, "Total balance didn't increase to 10");
+			return meta.deposit({
+				from:accounts[1], 
+				value:toWei(depositAmount)
+			});
+		}).then( ()=>{
+			
+			// Check final balances
+			accountFinal = toEth(getBalance(accounts[1]));
+			contractFinal = toEth(getBalance(meta.address));
+			return meta.showUserBalance.call(accounts[1]);
+		}).then( (userBalance)=>{
+			userBalanceFinal = toEth(userBalance);
+			
+			// Check assertions
+			assert.equal(userBalanceFinal-userBalanceInitial, depositAmount, 
+				`User balance didn't increase by ${depositAmount}, but went from ${userBalanceInitial} to ${userBalanceFinal}`);
+			assert.equal(contractFinal-contractInitial, depositAmount, 
+				`Contract balance didn't increase by ${depositAmount} eth, but went from ${contractInitial} to ${contractFinal}`);
+			//TODO: Account for spent gas
+			//assert.equal(accountInitial-accountFinal, depositAmount, 
+			//	`Account one balance didn't decrease by ${depositAmount} eth, but went from ${accountInitial} to ${accountFinal}`);
 		});
+		
 	});
 	
-	it("should not let the second account withdraw to the third", ()=>{
-		var meta;
+	it(`should not let the second account withdraw ${withdrawAmount} eth to the third`, ()=>{
 		return Mixer.deployed().then( (instance)=>{
 			meta = instance;
-			// Attempt withdraw
-			return meta.withdraw.call(accounts[2], 5, {from:accounts[1]});
-		})
-		.then(assert.fail)
-		.catch( (err)=>{
-			assert(err.message.indexOf("invalid opcode")>=0,"Didn't ensure the user has deposited that much funds")
-		})
-		.then( ()=>{
-			// Check total again
-			return meta.total.call();
-		})
-		.then( (total)=>{
-			assert.equal(total.toNumber(), 10, "Unauthorized account withdrew some funds");
+			
+			// Check initial balances
+			accountInitial = toEth(getBalance(accounts[3]));
+			contractInitial = toEth(getBalance(meta.address));
+			return meta.showUserBalance.call(accounts[1]);
+		}).then( (userBalance)=>{
+			userBalanceInitial = toEth(userBalance);
+			
+			// Attempt withdraw and check for failure
+			return meta.withdraw(
+				accounts[3], 
+				toWei(withdrawAmount), 
+				{from:accounts[2]}
+			);
+		}).then(assert.fail).catch( (err)=>{
+			assert(err.message.indexOf(
+				"invalid opcode")>=0,
+				"Didn't ensure the user has deposited that much funds"
+			)
+		}).then( ()=>{
+			
+			// Check final balances
+			accountFinal = toEth(getBalance(accounts[3]));
+			contractFinal = toEth(getBalance(meta.address));
+			return meta.showUserBalance.call(accounts[1]);
+		}).then( (userBalance)=>{
+			userBalanceFinal = toEth(userBalance);
+			
+			// Check assertions
+			assert.equal( accountInitial-accountFinal, 0,
+				`Account three balance changed from ${accountInitial} to ${accountFinal}, indicating possible unauthorized withdrawl`);
+			assert.equal( userBalanceInitial-userBalanceFinal, 0, 
+				`User balance for account one changed from ${userBalanceInitial} to ${userBalanceFinal}, indicating possible unauthorized withdrawl`);
+			assert.equal (contractInitial-contractFinal, 0, 
+				`Contract balance changed from ${contractInitial} to ${contractFinal}, indicating possible unauthorized withdrawl`);
 		});
+		
 	});
 	
-	it("should let the first account withdraw to the third", ()=>{
-		var meta;
+	it(`should let the first account withdraw ${withdrawAmount} eth to the third`, ()=>{
 		return Mixer.deployed().then( (instance)=>{
 			meta = instance;
+			
+			// Check initial balances
+			accountInitial = toEth(getBalance(accounts[3]));
+			contractInitial = toEth(getBalance(meta.address));
+			return meta.showUserBalance.call(accounts[1]);
+		}).then( (userBalance)=>{
+			userBalanceInitial = toEth(userBalance);
+			
 			// Attempt withdraw
-			return meta.withdraw(accounts[2], 5, {from:accounts[0]});
+			return meta.withdraw(
+				accounts[3], 
+				toWei(withdrawAmount), 
+				{ from: accounts[1] }
+			);
 		})
-		.then( ()=>{
-			// Check balance again
-			return meta.total.call();			
-		})
-		.then( (total)=>{
-			assert.equal(total.toNumber(), 5, "Unable to withdraw funds");
+		.then( (result)=>{
+			
+			// Check final balances
+			accountFinal = toEth(getBalance(accounts[3]));
+			contractFinal = toEth(getBalance(meta.address));
+			return meta.showUserBalance.call(accounts[1]);
+		}).then( (userBalance)=>{
+			userBalanceFinal = toEth(userBalance);
+			
+			assert.equal( contractInitial-contractFinal, withdrawAmount, 
+				`Contract balance didn't decrease by ${withdrawAmount} eth, but went from ${contractInitial} to ${contractFinal}`);
+			assert.equal( userBalanceInitial-userBalanceFinal, withdrawAmount, 
+				`User balance for account one didn't decrease by ${withdrawAmount}, but went from ${userBalanceInitial} to ${userBalanceFinal}`);
+			assert.equal( accountFinal-accountInitial, withdrawAmount, 
+				`Account three balance didn't increase by ${withdrawAmount} eth, but went from ${accountInitial} to ${accountFinal}`);
 		});
 	});
 
